681d7c3601df0de327cc5cf0f4716c520a304c23
Andrej Lohn <alohn@students.uni-mainz.de>
2018-11-11 13:42:01 +0100

    Commiting Mario Bros base.

---
+++ desktop/src/com/andrejlohn/mariobros/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.andrejlohn.mariobros.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.andrejlohn.mariobros.MarioBros;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MarioBros(), config);
+	}
+}
