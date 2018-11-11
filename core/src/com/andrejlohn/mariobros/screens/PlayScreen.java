6b551fa11822aeb357cc7e449e5e88c09157db1c
Andrej Lohn <alohn@students.uni-mainz.de>
2018-11-11 19:34:45 +0100

    Created bodies and fixtures for all in game objects using the Box2D class of libGDX.

--- core/src/com/andrejlohn/mariobros/screens/PlayScreen.java
+++ core/src/com/andrejlohn/mariobros/screens/PlayScreen.java
@@ -6,10 +6,20 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
@@ -28,9 +38,15 @@ public class PlayScreen implements Screen {
     private Viewport gamePort;
     private Hud hud;
 
+    // Tiled map
     private TmxMapLoader mapLoader;
     private TiledMap map;
     private OrthogonalTiledMapRenderer renderer;
+
+    // Box2D
+    private World world;
+    private Box2DDebugRenderer b2dr;
+
 
     /**
      * Creates the PlayScreen for a running MarioBros game. Sets up the game camera, viewport, HUD
@@ -51,16 +67,93 @@ public class PlayScreen implements Screen {
         hud = new Hud(game.batch);
 
         mapLoader = new TmxMapLoader();
-        map = mapLoader.load("level1.tmx");
+        map = mapLoader.load("level1_simple.tmx");
         renderer = new OrthogonalTiledMapRenderer(map);
         gameCam.position.set(
                 gamePort.getWorldWidth() / 2,
                 gamePort.getWorldHeight() / 2,
                 0);
+
+        world = new World(new Vector2(0, 0), true);
+        b2dr = new Box2DDebugRenderer();
+
+        BodyDef bDef = new BodyDef();
+        PolygonShape shape = new PolygonShape();
+        FixtureDef fDef = new FixtureDef();
+        Body body;
+
+        // Create ground  fixtures and bodies. The index of mapLayers().get() depends on the layers
+        // position in the .tmx file starting at 0.
+        for(MapObject object:
+                map.getLayers().get(2).getObjects().getByType(RectangleMapObject.class)) {
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+
+            bDef.type = BodyDef.BodyType.StaticBody;
+            bDef.position.set(
+                    rect.getX() + rect.getWidth() / 2,
+                    rect.getY() + rect.getHeight() / 2);
+
+            body = world.createBody(bDef);
+
+            shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+            fDef.shape = shape;
+            body.createFixture(fDef);
+        }
+
+        // Create pipe bodies/fixtures
+        for(MapObject object:
+                map.getLayers().get(3).getObjects().getByType(RectangleMapObject.class)) {
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+
+            bDef.type = BodyDef.BodyType.StaticBody;
+            bDef.position.set(
+                    rect.getX() + rect.getWidth() / 2,
+                    rect.getY() + rect.getHeight() / 2);
+
+            body = world.createBody(bDef);
+
+            shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+            fDef.shape = shape;
+            body.createFixture(fDef);
+        }
+
+        // Create brick bodies/fixtures
+        for(MapObject object:
+                map.getLayers().get(5).getObjects().getByType(RectangleMapObject.class)) {
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+
+            bDef.type = BodyDef.BodyType.StaticBody;
+            bDef.position.set(
+                    rect.getX() + rect.getWidth() / 2,
+                    rect.getY() + rect.getHeight() / 2);
+
+            body = world.createBody(bDef);
+
+            shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+            fDef.shape = shape;
+            body.createFixture(fDef);
+        }
+
+        // Create coin bodies/fixtures
+        for(MapObject object:
+                map.getLayers().get(4).getObjects().getByType(RectangleMapObject.class)) {
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+
+            bDef.type = BodyDef.BodyType.StaticBody;
+            bDef.position.set(
+                    rect.getX() + rect.getWidth() / 2,
+                    rect.getY() + rect.getHeight() / 2);
+
+            body = world.createBody(bDef);
+
+            shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+            fDef.shape = shape;
+            body.createFixture(fDef);
+        }
     }
 
     /**
-     * Handles user input wiht respect to the time passed since the last update.
+     * Handles user input with respect to the time passed since the last update.
      *
      * @param dt    the time passed
      * @see         Input#isTouched()
@@ -99,6 +192,7 @@ public class PlayScreen implements Screen {
      * @param delta the time passed
      * @see         OrthographicCamera#combined
      * @see         com.badlogic.gdx.graphics.g2d.SpriteBatch#setProjectionMatrix(Matrix4)
+     * @see         Box2DDebugRenderer#render(World, Matrix4)
      */
     @Override
     public void render(float delta) {
@@ -108,6 +202,8 @@ public class PlayScreen implements Screen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         renderer.render();
+
+        b2dr.render(world, gameCam.combined);
 
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
         hud.stage.draw();
