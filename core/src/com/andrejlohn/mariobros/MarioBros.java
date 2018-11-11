d7b569de4dc4a16386a10ac57098151624429999
Andrej Lohn <alohn@students.uni-mainz.de>
2018-11-11 14:35:29 +0100

    Added a camera and a viewport to the PlayScreen class. The viewport is a libGDX StretchViewport for testing this class in particular. Added documentation for all classes and implementaed function.

--- core/src/com/andrejlohn/mariobros/MarioBros.java
+++ core/src/com/andrejlohn/mariobros/MarioBros.java
@@ -4,21 +4,44 @@ import com.andrejlohn.mariobros.screens.PlayScreen;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+/**
+ * This represents the game main class. It runs the came loop of updating and rendering the game
+ * world to the device screen. This class extends the libGDX Game class.
+ *
+ * @version %I%, %G%
+ * @see Game
+ */
 public class MarioBros extends Game {
 	public SpriteBatch batch;
-	
-	@Override
+
+    /**
+     * Creates the Game. Sets up the SpriteBatch and the PlayScreen.
+     *
+     * @see SpriteBatch
+     * @see PlayScreen
+     */
+    @Override
 	public void create () {
 		batch = new SpriteBatch();
 		setScreen(new PlayScreen(this));
 	}
 
-	@Override
+    /**
+     * Renders the game to the device screen.
+     *
+     * @see Game#render()
+     */
+    @Override
 	public void render () {
 		super.render();
 	}
-	
-	@Override
+
+    /**
+     * Disposes all game elements not subject to the garbage collection. Prevents memory leak.
+     *
+     * @see SpriteBatch#dispose()
+     */
+    @Override
 	public void dispose () {
 		batch.dispose();
 	}
--- core/src/com/andrejlohn/mariobros/screens/PlayScreen.java
+++ core/src/com/andrejlohn/mariobros/screens/PlayScreen.java
@@ -4,16 +4,39 @@ import com.andrejlohn.mariobros.MarioBros;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
+/**
+ * This class represents the game screen in a running game. It implements the libGDX Screen
+ * interface. It contains the game camera and viewport.
+ *
+ * @version %I%, %G%
+ * @see     Screen
+ */
 public class PlayScreen implements Screen {
 
     private MarioBros game;
     Texture texture;
+    private OrthographicCamera gameCam;
+    private Viewport gamePort;
 
+    /**
+     * Creates the PlayScreen for a running MarioBros game. Sets up the game camera and viewport.
+     *
+     * @param game  the MarioBros game
+     * @see         OrthographicCamera
+     * @see         StretchViewport
+     */
     public PlayScreen(MarioBros game) {
         this.game = game;
         texture = new Texture("badlogic.jpg");
+        gameCam = new OrthographicCamera();
+        gamePort = new StretchViewport(800, 480, gameCam);
     }
 
     @Override
@@ -21,18 +44,36 @@ public class PlayScreen implements Screen {
 
     }
 
+    /**
+     * Renders the game to the device screen based on the time passed since the last rendering.
+     *
+     * @param delta the time passed
+     * @see         OrthographicCamera#combined
+     * @see         Batch#setProjectionMatrix(Matrix4)
+     * @see         Batch#begin()
+     * @see         Batch#draw(Texture, float, float)
+     * @see         Batch#end()
+     */
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.setProjectionMatrix(gameCam.combined);
         game.batch.begin();
         game.batch.draw(texture, 0, 0);
         game.batch.end();
     }
 
+    /**
+     * Resizes the viewport to a given screen size.
+     *
+     * @param width     the screen width
+     * @param height    the screen height
+     * @see             StretchViewport#update(int, int)
+     */
     @Override
     public void resize(int width, int height) {
-
+        gamePort.update(width, height);
     }
 
     @Override
